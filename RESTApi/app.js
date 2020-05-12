01cd5648b89c1878cb004ab4065bb1246aae7903
dosoung <soungdo0919@gmail.com>
2020-05-12 16:26:21 +0900

    CRUD REST-API

--- RESTApi/app.js
+++ RESTApi/app.js
@@ -1,26 +1,69 @@
+const path = require("path");
 const express = require("express");
 const PORT = 8080;
-
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
 const app = express();
+const uuid4 = require("uuid4"); //npm install uuid4
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuid4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // app.use(bodyParser.urlencoded()); //x-www-form-urlencoded
 app.use(bodyParser.json()); //application/json
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single("image"));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); //not send yet //* 어디서든 우리 서버에 접근 가능하다.
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, PUT, PATCH, DELETE",
-  // );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE",
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 app.use("/feed", feedRoutes);
 
-app.listen(PORT);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
-//solve cors error cors error is 서버가 다를때 발생한다.
+mongoose
+  .connect(
+    "mongodb+srv://David:!sdh0919@cluster0-ozgw6.mongodb.net/messages?retryWrites=true&w=majority",
+    () => {
+      console.log("Connected DB");
+    },
+  )
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log(`Running on Server ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
